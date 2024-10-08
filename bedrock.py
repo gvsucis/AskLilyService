524b830d92ceda120bf96a38c1e013ad34ec9f08
Luis Gomez <lgomezm90@gmail.com>
2024-10-07 21:39:00 -0400

    Simplified requirements for bedrock.py

--- bedrock.py
+++ bedrock.py
@@ -1,20 +1,13 @@
 import os
+
 import boto3
 from dotenv import load_dotenv
-from langchain_community.retrievers import YouRetriever
-from langchain.chains import RetrievalQA
-from langchain_community.llms import Bedrock
-from langchain_community.chat_models import BedrockChat
-from langchain.memory import ConversationBufferWindowMemory
-from langchain.memory.chat_message_histories import RedisChatMessageHistory
-
-import os
-from dotenv import load_dotenv
-from langchain_community.retrievers import YouRetriever
 from langchain.chains import ConversationChain
-from langchain_community.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferWindowMemory
 from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_community.chat_models import BedrockChat
+from langchain_community.retrievers import YouRetriever
+
 
 load_dotenv()
 
@@ -22,8 +15,7 @@ redis_host = os.getenv("REDIS_HOST", "localhost")
 redis_port = int(os.getenv("REDIS_PORT", 6379))
 redis_password = os.getenv("REDIS_PASSWORD", "eYVX7EwVmmxKPCDmwMtyKVge8oLd2t81")
 
-# Set up Redis-backed memory
-session_id = "user_3"  # Generate or retrieve a unique session ID
+session_id = "user_4"
 
 message_history = RedisChatMessageHistory(
     session_id, url=f"redis://:{redis_password}@{redis_host}:{redis_port}"
@@ -51,18 +43,15 @@ bedrock_model = BedrockChat(
     },
 )
 
-# Create a ConversationChain instance without the retriever
 conversation_chain = ConversationChain(
     llm=bedrock_model,
     memory=memory,
 )
 
-# Initialize the retriever
 yr = YouRetriever()
 
 
 def get_response_with_retrieval(query):
-    # Fetch relevant information using the retriever
     retrieved_info = yr.results("GVSU: " + query)
 
     prompt_template = (
@@ -80,7 +69,6 @@ def get_response_with_retrieval(query):
     return conversation_chain(full_prompt)
 
 
-# Use the conversation chain with retrieval logic
 response = get_response_with_retrieval("Who is Jonathan Engelsma?")
 print(response["response"])
 
