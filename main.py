c5467fa2395dadb3c129fb49e18304d03f8089e1
Luis Gomez <lgomezm90@gmail.com>
2024-10-09 22:59:56 -0400

    Wired up with FastAPI

--- main.py
+++ main.py
@@ -1,18 +1,37 @@
 import os
 
+import boto3
 from dotenv import load_dotenv
-from fastapi import FastAPI, HTTPException
-from langchain_community.retrievers import YouRetriever
-from langchain.chains import RetrievalQA
-from langchain_community.chat_models import ChatOpenAI
+from fastapi import FastAPI
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferWindowMemory
+from langchain_community.chat_message_histories import RedisChatMessageHistory
+from langchain_community.chat_models import BedrockChat
+from langchain_google_community import GoogleSearchAPIWrapper
 from pydantic import BaseModel
 
 load_dotenv()
 
-yr = YouRetriever()
-model = "gpt-3.5-turbo-16k"
-qa = RetrievalQA.from_chain_type(
-    llm=ChatOpenAI(model=model, max_tokens=100), chain_type="stuff", retriever=yr
+search = GoogleSearchAPIWrapper()
+
+redis_host = os.getenv("REDIS_HOST")
+redis_port = int(os.getenv("REDIS_PORT"))
+redis_password = os.getenv("REDIS_PASSWORD")
+
+bedrock_runtime = boto3.client(
+    service_name="bedrock-runtime",
+    region_name="us-east-1",
+)
+
+bedrock_model = BedrockChat(
+    model_id="amazon.titan-text-lite-v1",
+    client=bedrock_runtime,
+    model_kwargs={
+        "maxTokenCount": 256,
+        "stopSequences": [],
+        "temperature": 0,
+        "topP": 1,
+    },
 )
 
 app = FastAPI()
@@ -20,14 +39,37 @@ app = FastAPI()
 
 class ChatRequest(BaseModel):
     message: str
+    session_id: str
 
 
 @app.post("/chat")
 def query_openai(request: ChatRequest):
-    if not request.message:
-        raise HTTPException(status_code=400, detail={"msg": "Message is required"})
-    try:
-        response = qa.run(f"You are an assistant at GVSU. {request.message}")
-        return {"response": response}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+    message_history = RedisChatMessageHistory(
+        request.session_id, url=f"redis://:{redis_password}@{redis_host}:{redis_port}"
+    )
+
+    memory = ConversationBufferWindowMemory(
+        memory_key="history",
+        chat_memory=message_history,
+        k=3,  # Remember last 3 interactions
+    )
+    conversation_chain = ConversationChain(
+        llm=bedrock_model,
+        memory=memory,
+    )
+    retrieved_info = search.results(request.message, 5)
+
+    prompt_template = (
+        "You are a helpful assistant. Using the following information: {retrieved_info}\n"
+        "Answer the user's question: {user_query}"
+    )
+
+    full_prompt = prompt_template.format(
+        retrieved_info=(
+            retrieved_info if retrieved_info else "No relevant information found."
+        ),
+        user_query=request.message,
+    )
+
+    response = conversation_chain(full_prompt)
+    return {"response": response["response"]}
