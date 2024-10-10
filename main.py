3b93a25f39cdd7684ac92fc4520a02414499845e
Luis Gomez <lgomezm90@gmail.com>
2024-10-09 23:26:37 -0400

    Using AWS credentials from env vars

--- main.py
+++ main.py
@@ -16,6 +16,8 @@ search = GoogleSearchAPIWrapper()
 
 bedrock_runtime = boto3.client(
     service_name="bedrock-runtime",
+    aws_access_key_id=os.getenv("AWS_ACCESS_KEY"),
+    aws_secret_access_key=os.getenv("AWS_SECRET_KEY"),
     region_name="us-east-1",
 )
 
